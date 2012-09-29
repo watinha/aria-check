b5c56567a79d0416fa78be305595995536aac96e
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-09-28 23:32:05 -0300

    submit first dummy jasmine test suite

---
+++ data/lib.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * The Globale Variables
+ */
+
+if (!window.Node) {
+  var Node = {            // If there is no Node object, define one
+    ELEMENT_NODE: 1,    // with the following properties and values.
+    ATTRIBUTE_NODE: 2,  // Note that these are HTML node types only.
+    TEXT_NODE: 3,       // For XML-specific nodes, you need to add
+    COMMENT_NODE: 8,    // other constants here.
+    DOCUMENT_NODE: 9,
+    DOCUMENT_FRAGMENT_NODE: 11
+  }
+}
+
+
+var KEY_PAGEUP   = 33;
+var KEY_PAGEDOWN = 34;
+var KEY_END      = 35;
+var KEY_HOME     = 36;
+
+var KEY_LEFT     = 37;
+var KEY_UP       = 38;
+var KEY_RIGHT    = 39;
+var KEY_DOWN     = 40;
+
+var KEY_SPACE    = 32;
+var KEY_TAB      = 9;
+
+var KEY_BACKSPACE = 8;
+var KEY_DELETE    = 46;
+var KEY_ENTER     = 13;
+var KEY_INSERT    = 45;
+var KEY_ESCAPE    = 27;
+
+var KEY_F1        = 112;
+var KEY_F2        = 113;
+var KEY_F3        = 114;
+var KEY_F4        = 115;
+var KEY_F5        = 116;
+var KEY_F6        = 117;
+var KEY_F7        = 118;
+var KEY_F8        = 119;
+var KEY_F9        = 120;
+var KEY_F10       = 121;
+
+var KEY_M         = 77;
+
+
