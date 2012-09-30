d5dfa5ece3de41416852d116eab8cd1a0273e96d
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-09-30 15:38:44 -0300

    just covering onclick callback functionality for the widget

---
+++ test/test-activation_widget.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+
+    exports["test create instance should consider onclick callback passed as parameter"] = function (assert) {
+        var activation_widget = require("activation_widget"),
+            widget_instance,
+            widget_mock = {
+                Widget: function (params) {
+                    return params;
+                }
+            },
+            callbackCalled = false,
+            callbackHandler = {
+                callback: function () {
+                    callbackCalled = true;
+                    // assert that the context of the callback is the same as passed
+                    assert.assertEqual(callbackHandler, this);
+                }
+            };
+
+        activation_widget.clear_instance(); // test method for clearing singleton instance
+        widget_instance = activation_widget.create_instance(widget_mock, function () {
+            callbackHandler.callback()
+        });
+        widget_instance.onClick.call({});
+        assert.assert(callbackCalled);
+    };
+
+}());
