5e8be0f9115f78985df0042dd329086db0094d11
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-09-30 15:15:06 -0300

    now using non-camelcase standard for coding

--- test/test-app.js
+++ test/test-app.js
@@ -2,8 +2,8 @@
     'use strict';
 
     exports["test init should be a singleton for widgets objects"] = function (assert) {
-        var app1 = require("app").createApp(),
-            app2 = require("app").createApp();
+        var app1 = require("app").create_app(),
+            app2 = require("app").create_app();
 
         app1.init();
         app2.init();
@@ -12,7 +12,7 @@
     };
 
     exports["test init should instantiate a widget object with the correct parameters"] = function (assert) {
-        var app = require("app").createApp();
+        var app = require("app").create_app();
         app.init();
         assert.assertNotUndefined(app.widget, "widget object should be set");
         assert.assertEqual("aria-check-widget", app.widget.id);
