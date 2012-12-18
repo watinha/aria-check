b133e45786a70967b50b1dd400f24388749d8e3e
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-12-18 14:42:16 -0200

    refactor the error messages and tests

--- data/tests/tabpanel_cases.js
+++ data/tests/tabpanel_cases.js
@@ -1,12 +1,12 @@
 (function () {
     "use strict";
 
-    describe("tab behavior", function () {
+    describe("Tab widget: ", function () {
 
         /* check if tab related roles are in the page */
-        it("should have a tab role in the page", function () {
+        it("the webpage should have a tab role in the page", function () {
             var tabElements = document.querySelectorAll("*[role='tab']");
-            expect(tabElements).toBeGreaterThan(0);
+            expect(tabElements.length).toBeGreaterThan(0);
         });
 
     });
