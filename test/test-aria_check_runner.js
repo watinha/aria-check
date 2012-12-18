569c9c48586d430edc5ffafaa0e3da7e28a83f5f
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-12-18 13:06:19 -0200

    a little refactor just to make behavior testing easier

--- test/test-aria_check_runner.js
+++ test/test-aria_check_runner.js
@@ -1,37 +1,32 @@
 (function () {
     "use strict";
+    var TestHelpers = {
+        check_report: function (assert, done, fixture, report_handler) {
+            var tabs = require("tabs"),
+                self = require("self"),
+                aria_check_runner = require("aria_check_runner");
+
+            tabs.open({
+                url: self.data.url(fixture),
+                onReady: function () {
+                    var worker = aria_check_runner.execute();
+                    worker.port.on("jasmine_report", report_handler);
+                }
+            });
+        }
+    };
 
     exports["test jasmine report transport from contentScript to chrome"] = function (assert, done) {
-        var tabs = require("tabs"),
-            self = require("self"),
-            aria_check_runner = require("aria_check_runner");
-
-        tabs.open({
-            url: self.data.url("fixtures/tabpanel/tabpanel_dummy.html"),
-            onReady: function () {
-                var worker = aria_check_runner.execute();
-                worker.port.on("jasmine_report", function (data) {
-                    assert.ok("worker should have a data reporter");
-                    done();
-                });
-            }
+        TestHelpers.check_report(assert, done, "fixtures/tabpanel/tabpanel_dummy.html", function (data) {
+            assert.ok("worker should have a data reporter");
+            done();
         });
     };
 
     exports["test should be reported that there is no tab widget in tabpanel_dummy"] = function (assert, done) {
-        var tabs = require("tabs"),
-            self = require("self"),
-            aria_check_runner = require("aria_check_runner");
-
-        tabs.open({
-            url: self.data.url("fixtures/tabpanel/tabpanel_dummy.html"),
-            onReady: function () {
-                var worker = aria_check_runner.execute();
-                worker.port.on("jasmine_report", function (data) {
-                    assert.ok(!data.passed, "the test should fail since there are no tab widgets in the webpage");
-                    done();
-                });
-            }
+        TestHelpers.check_report(assert, done, "fixtures/tabpanel/tabpanel_dummy.html", function (data) {
+            assert.ok(!data.passed, "the test should fail since there are no tab widgets in the webpage");
+            done();
         });
     };
 
