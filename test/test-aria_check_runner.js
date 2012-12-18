7168bffc89ce4133f96a74d8f8377d343cf2af38
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-12-18 12:57:25 -0200

    now we should focus on adding the test cases for verifying tabs accessibility

--- test/test-aria_check_runner.js
+++ test/test-aria_check_runner.js
@@ -18,5 +18,22 @@
         });
     };
 
+    exports["test should be reported that there is no tab widget in tabpanel_dummy"] = function (assert, done) {
+        var tabs = require("tabs"),
+            self = require("self"),
+            aria_check_runner = require("aria_check_runner");
+
+        tabs.open({
+            url: self.data.url("fixtures/tabpanel/tabpanel_dummy.html"),
+            onReady: function () {
+                var worker = aria_check_runner.execute();
+                worker.port.on("jasmine_report", function (data) {
+                    assert.ok(!data.passed, "the test should fail since there are no tab widgets in the webpage");
+                    done();
+                });
+            }
+        });
+    };
+
     require("test").run(exports);
 }());
