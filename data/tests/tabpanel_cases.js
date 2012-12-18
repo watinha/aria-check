4831c01805dda73707d6c695fc45252cea57ceea
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2012-12-18 17:15:25 -0200

    little refactor to include semantic nested describe blocks

--- data/tests/tabpanel_cases.js
+++ data/tests/tabpanel_cases.js
@@ -4,63 +4,69 @@
     describe("Tab widget: ", function () {
 
         /* check if tab related roles are in the page */
-        it("the webpage should have a tab role in the page", function () {
-            var tabElements = document.querySelectorAll("*[role='tab']");
-            expect(tabElements.length).toBeGreaterThan(0);
-        });
+        describe("tab related roles: ", function () {
+            it("the webpage should have a tab role in the page", function () {
+                var tabElements = document.querySelectorAll("*[role='tab']");
+                expect(tabElements.length).toBeGreaterThan(0);
+            });
 
-        it("the webpage should have a tablist role element that contains the tab role element", function () {
-            var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']");
-            expect(tabElements.length).toBeGreaterThan(0);
-        });
+            it("the webpage should have a tablist role element that contains the tab role element", function () {
+                var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']");
+                expect(tabElements.length).toBeGreaterThan(0);
+            });
 
-        it("the webpage should have a tabpanel role element in the page", function () {
-            var tabElements = document.querySelectorAll("*[role='tabpanel']");
-            expect(tabElements.length).toBeGreaterThan(0);
+            it("the webpage should have a tabpanel role element in the page", function () {
+                var tabElements = document.querySelectorAll("*[role='tabpanel']");
+                expect(tabElements.length).toBeGreaterThan(0);
+            });
         });
 
         /* checks initial interaction stats on tab elements */
-        it("there should be one focusable tab role element when the page loads", function () {
-            var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
-                focusable = [];
+        describe("initial interaction state: ", function () {
+            it("there should be one focusable tab role element when the page loads", function () {
+                var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
+                    focusable = [];
 
-            for (var i = 0; i < tabElements.length; i++) {
-                if (tabElements[i].tabIndex >= 0) {
-                    focusable.push(tabElements[i]);
-                }
-            };
+                for (var i = 0; i < tabElements.length; i++) {
+                    if (tabElements[i].tabIndex >= 0) {
+                        focusable.push(tabElements[i]);
+                    }
+                };
 
-            expect(focusable.length).toBeGreaterThan(0);
-        });
+                expect(focusable.length).toBeGreaterThan(0);
+            });
 
-        it("there should be one tabpanel role element for each tab role elements", function () {
-            var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
-                tabPanelElements = document.querySelectorAll("*[role='tabpanel']");
+            it("there should be one tabpanel role element for each tab role elements", function () {
+                var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
+                    tabPanelElements = document.querySelectorAll("*[role='tabpanel']");
 
-            expect(tabElements.length).toBe(tabPanelElements.length);
-        });
+                expect(tabElements.length).toBe(tabPanelElements.length);
+            });
 
-        it("there should be only one interactive tab role element", function () {
-            var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
-                focusable = [];
+            it("there should be only one interactive tab role element", function () {
+                var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']"),
+                    focusable = [];
 
-            for (var i = 0; i < tabElements.length; i++) {
-                if (tabElements[i].tabIndex >= 0) {
-                    focusable.push(tabElements[i]);
-                }
-            };
+                for (var i = 0; i < tabElements.length; i++) {
+                    if (tabElements[i].tabIndex >= 0) {
+                        focusable.push(tabElements[i]);
+                    }
+                };
 
-            expect(focusable.length).toBe(1);
+                expect(focusable.length).toBe(1);
+            });
         });
 
         /* check the text labels for the tab elements */
-        it("there should be descriptive text content for each tab role element", function () {
-            var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']");
+        describe("text alternatives", function () {
+            it("there should be descriptive text content for each tab role element", function () {
+                var tabElements = document.querySelectorAll("*[role='tablist'] *[role='tab']");
 
-            for (var i = 0; i < tabElements.length; i++) {
-                expect(tabElements[i].textContent).not.toBe("");
-                expect(tabElements[i].textContent.length).not.toBe(0);
-            };
+                for (var i = 0; i < tabElements.length; i++) {
+                    expect(tabElements[i].textContent).not.toBe("");
+                    expect(tabElements[i].textContent.length).not.toBe(0);
+                };
+            });
         });
     });
 }());
