dbc84b0ec13784fa070484a1b0f9f1d9a8525517
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-08-25 23:10:39 -0300

    WIP: jasmine env recovered, given failure in the simulation of keyboard events inside content scripts contexts (started in version 1.10 of the ADDON-SDK)

--- data/jasmine/js/jasmine-activation.js
+++ data/jasmine/js/jasmine-activation.js
@@ -21,54 +21,42 @@
     };
 
     (function () {
-        self.port.on("jasmineCSSURL", function (data) {
+        jasmineEnv.execute();
+
+        /**
+          * sends a json reporter back to chrome privileged scripts
+          */
+        (function () {
+            var jsReport,
+                verifyReporter = function () {
+                    if (jasmine.getJSReport)
+                        self.port.emit("jasmine_report", jasmine.getJSReport());
+                    else
+                        setTimeout(verifyReporter, 2000);
+                };
+            verifyReporter();
+        })();
+
+        /**
+          * set a cool layout for the reporter
+          */
+        (function () {
             var reporter = document.querySelector("#HTMLReporter"),
-                link = document.createElement("link");
-            if (reporter) {
-                reporter.parentNode.removeChild(reporter);
-            } else {
-                link.type = "text/css";
-                link.rel = "stylesheet";
-                link.href = data;
-                document.body.appendChild(link);
+                overlay = document.querySelector("#HTMLReporterOverlay");
+
+            if (!overlay) {
+                overlay = document.createElement("div");
+                overlay.id = "HTMLReporterOverlay";
+                overlay.style.cssText = "background-color:black;opacity:0.8;position:fixed;top:0;left:0;width:100%;height:100%;z-index:0;";
+                overlay.addEventListener("click", function () {
+                    document.body.removeChild(overlay);
+                    document.body.removeChild(reporter);
+                    document.body.removeChild(link);
+                });
+                document.body.appendChild(overlay);
             }
-            jasmineEnv.execute();
-
-            /**
-              * sends a json reporter back to chrome privileged scripts
-              */
-            (function () {
-                var jsReport,
-                    verifyReporter = function () {
-                        if (jasmine.getJSReport)
-                            self.port.emit("jasmine_report", jasmine.getJSReport());
-                        else
-                            setTimeout(verifyReporter, 2000);
-                    };
-                verifyReporter();
-            })();
-
-            /**
-              * set a cool layout for the reporter
-              */
-            (function () {
-                var reporter = document.querySelector("#HTMLReporter"),
-                    overlay = document.querySelector("#HTMLReporterOverlay");
-
-                if (!overlay) {
-                    overlay = document.createElement("div");
-                    overlay.id = "HTMLReporterOverlay";
-                    overlay.style.cssText = "background-color:black;opacity:0.8;position:fixed;top:0;left:0;width:100%;height:100%;z-index:0;";
-                    overlay.addEventListener("click", function () {
-                        document.body.removeChild(overlay);
-                        document.body.removeChild(reporter);
-                        document.body.removeChild(link);
-                    });
-                    document.body.appendChild(overlay);
-                }
-                reporter.style.cssText = "position:fixed;top:5%;left:5%;padding:5% 5%;height:70%;width:80%;z-index:1;background-color:#EEEEEE;overflow-y:scroll";
-            }());
-        });
+            reporter.style.cssText = "position:fixed;top:5%;left:5%;padding:5% 5%;height:70%;width:80%;z-index:1;background-color:#EEEEEE;overflow-y:scroll";
+        }());
     }());
 })();
 
