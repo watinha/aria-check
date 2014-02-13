b9b6af3aae630d0b44038d70dfff7acfbe51137d
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-02-13 19:48:41 -0200

    in fixture tabpanel_dummy3 (just with roles verifications)

--- selenium_tests/TestTabPanel.py
+++ selenium_tests/TestTabPanel.py
@@ -1,7 +1,27 @@
 import unittest
 import os
 
+from selenium import webdriver
+
 class TestRolesVerifications (unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get("file://" + os.environ.get("PWD") + "/" + os.environ.get("URL"))
+
     def test_are_there_tabs (self):
-        self.assertEquals('selenium_tests/tabpanel', os.environ.get("URL"))
+        tabs = self.browser.find_elements_by_css_selector("[role=tab]")
+        self.assertGreaterEqual(len(tabs), 1)
+
+    def test_are_there_tabpanels (self):
+        tabs = self.browser.find_elements_by_css_selector("[role=tabpanel]")
+        self.assertGreaterEqual(len(tabs), 1)
+
+    def test_are_there_tablists (self):
+        tabs = self.browser.find_elements_by_css_selector("[role=tablist]")
+        self.assertGreaterEqual(len(tabs), 1)
+
+    @classmethod
+    def tearDownClass(self):
+        self.browser.quit()
